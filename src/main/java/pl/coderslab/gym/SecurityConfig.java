2b8a08ed69a68e5b1b5570f9eff75f1b8e3f2290
PiotrSciepko <piotrek.priv@gmail.com>
2020-12-07 21:52:22 +0100

    modify access rules SecurityConfig.java

--- src/main/java/pl/coderslab/gym/SecurityConfig.java
+++ src/main/java/pl/coderslab/gym/SecurityConfig.java
@@ -12,7 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/person/**").hasRole("user")
+                .antMatchers("/**/list").permitAll()
+                .anyRequest().authenticated()
                 .and().formLogin();
     }
 }
