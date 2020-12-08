37ac4bdcd1435f7510422def117a1c030489c7e8
PiotrSciepko <piotrek.priv@gmail.com>
2020-12-08 10:32:27 +0100

    add admin role for delete

--- src/main/java/pl/coderslab/gym/SecurityConfig.java
+++ src/main/java/pl/coderslab/gym/SecurityConfig.java
@@ -1,6 +1,7 @@
 package pl.coderslab.gym;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**/add/**", "/**/update/**", "/**/delete/**", "/**/show/**").hasRole("user")
+                .antMatchers("/**/add/**", "/**/update/**", "/**/show/**").hasRole("USER")
+                .antMatchers("/**/delete/**").hasRole("ADMIN")
                 .and().formLogin();
     }
 }
